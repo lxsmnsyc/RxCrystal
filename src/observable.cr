b502c8278097a773581b38f6302c86faf7848922
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-24 10:18:53 +0800

    Update observable.cr

--- src/observable.cr
+++ src/observable.cr
@@ -2,76 +2,19 @@ require "./cancellable"
 require "./emitter"
 require "./observer"
 
-require "./operators/observable/create"
+require "./operators/observable/*"
 
-include Observer
-include Cancellable
-include Emitter
+include ObserverModule
+include CancellableModule
+include EmitterModule
 
-module Observable
-  private class LambdaObserver(T) < ObservableObserver(T)
-    @state : LinkedCancellable
-
-    @next : T -> Nil
-    @error : Exception -> Nil
-    @complete : Proc(Nil)
-
-    def initialize(@next : T -> Nil, @error : Exception -> Nil, @complete : Proc(Nil))
-      @state = LinkedCancellable.new
-    end
-
-    def onSubscribe(x : Cancellable::Cancellable)
-      @state.link x
-    end
-
-    def onComplete
-      @complete.call
-    end
-
-    def onError(x : Exception)
-      @error.call x
-    end
-
-    def onNext(x : T)
-      @next.call x
-    end
-
-    def state
-      @state
-    end
-  end
-
+module ObservableModule
+  extend self
   abstract class Observable(T)
-    def self.create(subscriber : ObservableEmitter(T) -> Nil)
-      ObservableCreate::ObservableCreate.new subscriber
-    end
-
-    def |(operator : Observable(T) -> Observable(T))
-      operator.call self
-    end
-
     abstract def subscribeActual(observer : ObservableObserver(T))
 
     def subscribeWith(observer : ObservableObserver(T))
       subscribeActual observer
     end
-
-    def subscribe(onNext : T -> Nil, onError : Exception -> Nil, onComplete : Proc(Nil))
-      observer = LambdaObserver(T).new(onNext, onError, onComplete)
-      state = observer.state
-      subscribeWith observer
-      return state
-    end
   end
 end
-Observable::Observable(String).create(->(e: ObservableEmitter(String)){
-  e.onNext("Hello")
-  e.onNext("World")
-  e.onComplete
-})
-
-.subscribe(
-  ->(x : String){ puts x },
-  ->(x : Exception) { puts x },
-  ->{ puts "Completed" }
-)
