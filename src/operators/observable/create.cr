a6f7656ccd780081e9161c33afdf0a1cd2cdb0fc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-24 10:19:01 +0800

    Update create.cr

--- src/operators/observable/create.cr
+++ src/operators/observable/create.cr
@@ -2,22 +2,20 @@ require "../../observer"
 require "../../emitter"
 require "../../observable"
 
-module ObservableCreate
-  class ObservableCreate(T) < Observable::Observable(T)
-    def initialize(@subscriber : ObservableEmitter(T) -> Nil)
-      super
-    end
+module ObservableCreate(T)
+  def subscribeActual(observer : ObservableObserver(T))
+    emitter = ObservableEmitter(T).new observer
 
-    def subscribeActual(observer : ObservableObserver(T))
-      emitter = ObservableEmitter(T).new observer
+    observer.onSubscribeemitter
 
-      observer.onSubscribe(emitter)
+    begin
+      @subscriber.call emitter
+    rescue ex
+      emitter.onError ex
+    end 
+  end
 
-      begin
-        @subscriber.call emitter
-      rescue ex
-        emitter.onError(ex)
-      end 
-    end
+  def create(subscriber : ObservableEmitter(T) -> Nil) : Observable::Observable(T)
+    
   end
 end
