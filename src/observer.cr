190d4dd839e481c5ae8e8a5c6740a40d0c955123
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-14 11:56:54 +0800

    Update observer.cr

--- src/observer.cr
+++ src/observer.cr
@@ -1,22 +1,30 @@
 require "./cancellable"
 
-module ObserverModule
-  abstract class GenericObserver
-    abstract def onSubscribe(x : Cancellable::Cancellable)
-    abstract def onError(x : Exception)
-  end
-  abstract class ObservableObserver(T) < GenericObserver
-    abstract def onNext(x : T)
-    abstract def onComplete
-  end
-  abstract class MaybeObserver(T) < GenericObserver
-    abstract def onSuccess(x : T)
-    abstract def onComplete
-  end
-  abstract class SingleObserver(T) < GenericObserver
-    abstract def onSuccess(x : T)
-  end
-  abstract class CompletableObserver < GenericObserver
-    abstract def onComplete
-  end
+module Observer
+  abstract def onSubscribe(c : Cancellable)
+end
+
+module GenericObserver
+  include Observer
+  abstract def onError(x : Exception)
+end
+
+module SingleObserver(T)
+  include GenericObserver
+  abstract def onSuccess(x : T)
+end
+
+module CompletableObserver
+  include GenericObserver
+  abstract def onComplete
+end
+
+module MaybeObserver(T)
+  include SingleObserver(T)
+  include CompletableObserver
+end
+
+module ObservableObserver(T)
+  include CompletableObserver
+  abstract def onNext(x : T)
 end
