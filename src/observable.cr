e09275eccee00712de08f3b4844c269628656329
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-14 11:56:52 +0800

    Update observable.cr

--- src/observable.cr
+++ src/observable.cr
@@ -1,20 +1,12 @@
-require "./cancellable"
-require "./emitter"
+require "./transformer"
+require "./operator"
 require "./observer"
 
-require "./operators/observable/*"
-
-include ObserverModule
-include CancellableModule
-include EmitterModule
-
-module ObservableModule
-  extend self
-  abstract class Observable(T)
-    abstract def subscribeActual(observer : ObservableObserver(T))
-
-    def subscribeWith(observer : ObservableObserver(T))
-      subscribeActual observer
-    end
+abstract class Observable(T)
+  def |(transformer : ObservableTransformer(U, D)) : Observable(D)
+    transformer.apply(self)
   end
+
+  abstract def subscribeActual(observer : ObservableObserver(T))
 end
+
