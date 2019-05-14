e9e510f9d8d506272e5bd3cb5ea49995b03b27aa
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-14 11:57:30 +0800

    Create transformer.cr

---
+++ src/transformer.cr
@@ -0,0 +1,20 @@
+require "./single"
+require "./maybe"
+require "./completable"
+require "./observable"
+
+module SingleTransformer(U, D)
+  abstract def apply(upstream : Single(U)) : Single(D)
+end
+
+module CompletableTransformer
+  abstract def apply(upstream : Completable) : Completable
+end
+
+module MaybeTransformer(U, D)
+  abstract def apply(upstream : Maybe(U)) : Maybe(D)
+end
+
+module ObservableTransformer(U, D)
+  abstract def apply(upstream : Observable(U)) : Observable(D)
+end
