a9295a972bf3744ce14e712c6e6f2624dcfcd73b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 09:38:11 +0800

    Update maybe.cr

--- src/maybe.cr
+++ src/maybe.cr
@@ -2,7 +2,13 @@ require "./transformer"
 require "./operator"
 require "./observer"
 
+abstract class MaybeSource(T)
+  abstract def subscribe(observer : MaybeObserver(T))
+end
+
 abstract class Maybe(T)
+  include MaybeSource(T)
+
   def |(transformer : MaybeTransformer(U, D)) : Maybe(D)
     transformer.apply(self)
   end
