eff8984a26280ce6c990290167ddbe3565e7034b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 09:38:19 +0800

    Create OnCompleteMaybeObserver.cr

---
+++ src/observers/maybe/OnCompleteMaybeObserver.cr
@@ -0,0 +1,52 @@
+require "../../observer"
+require "../../cancellable"
+require "../../subscription"
+
+class OnCompleteMaybeObserver(T) < MaybeObserver(T)
+  include Cancellable
+
+  @upstream : Proc(Void)
+  @withSubscription : Bool
+  @state : Subscription
+
+  def initialize(@upstream : Proc(Void))
+    @state = BasicSubscription.new
+  end
+
+  def cancel
+    @state.cancel()
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+  def onSuccess(x : T)
+    if (@withSubscription && @alive)
+      @state.cancel()
+    end
+  end
+
+  def onComplete
+    if (@withSubscription && @alive)
+      begin
+        @upstream.call()
+      ensure
+        @state.cancel()
+      end
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      @state.cancel()
+    else
+      raise e
+    end
+  end
+end
