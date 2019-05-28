e99b4080b6e2a7f467e7c9277ba497bcda41bd81
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 09:38:35 +0800

    Update single.cr

--- src/single.cr
+++ src/single.cr
@@ -2,7 +2,13 @@ require "./transformer"
 require "./operator"
 require "./observer"
 
+abstract class SingleSource(T)
+  abstract def subscribe(observer : SingleObserver(T))
+end
+
 abstract class Single(T)
+  include SingleSource(T)
+
   def |(transformer : SingleTransformer(U, D)) : Single(D)
     transformer.apply(self)
   end
