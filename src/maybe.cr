9ee3cef7095ffbfe89cedb1fa0583242e78fc5a8
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 12:26:31 +0800

    Update maybe.cr

--- src/maybe.cr
+++ src/maybe.cr
@@ -1,8 +1,10 @@
 require "./transformer"
 require "./operator"
 require "./observer"
+require "./subscription"
+require "./observers/maybe/**"
 
-abstract class MaybeSource(T)
+module MaybeSource(T)
   abstract def subscribe(observer : MaybeObserver(T))
 end
 
@@ -14,5 +16,34 @@ abstract class Maybe(T)
   end
 
   abstract def subscribeActual(observer : MaybeObserver(T))
+
+  def subscribeWith(observer : MaybeObserver(T)) : MaybeObserver(T)
+    self.subscribeActual(observer)
+    return observer
+  end
+
+  def subscribe(observer : MaybeObserver(T)) : Subscription
+    observer = CancellableMaybeObserver.new(observer)
+    self.subscribeActual(observer)
+    return observer
+  end
+
+  def subscribe(onSuccess : Proc(T, Nil)) : Subscription
+    observer = OnSuccessMaybeObserver.new(onSuccess)
+    self.subscribeActual(observer)
+    return observer
+  end
+
+  def subscribe(onSuccess : Proc(T, Nil), onError : Proc(Exception, Nil)) : Subscription
+    observer = onErrorMaybeObserver.new(onSuccess, onError)
+    self.subscribeActual(observer)
+    return observer
+  end
+
+  def subscribe(onSuccess : Proc(T, Nil), onComplete : Proc(Void), onError : Proc(Exception, Nil)) : Subscription
+    observer = LambdaMaybeObserver.new(onSuccess, onComplete, onError)
+    self.subscribeActual(observer)
+    return observer
+  end
 end
 
