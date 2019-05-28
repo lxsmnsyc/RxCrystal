e2ce1dd859fd82af55bba117d1eb230ec7409f09
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 16:01:20 +0800

    Create OnCompleteCompletableObserver.cr

---
+++ src/observers/completable/OnCompleteCompletableObserver.cr
@@ -0,0 +1,52 @@
+require "../../RxCrystal"
+
+class OnCompleteCompletableObserver(T)
+  include CompletableObserver(T)
+  include Cancellable
+
+  @upstream : (Proc(Void))?
+  @withSubscription : Bool
+  @state : Subscription
+  @alive : Bool
+
+  def initialize(@upstream : Proc(Void))
+    @state = BasicSubscription.new
+    @withSubscription = false
+    @alive = true
+  end
+
+  def cancel
+    if (@alive)
+      @alive = false
+      @state.cancel()
+    end
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+
+  def onComplete
+    if (@withSubscription && @alive)
+      begin
+        @upstream.call()
+      ensure
+        self.cancel()
+      end
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      self.cancel()
+    else
+      raise e
+    end
+  end
+end
