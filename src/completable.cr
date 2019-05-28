93680a69380ff31b7b29df49e7cf8b576b93da3c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 09:38:06 +0800

    Update completable.cr

--- src/completable.cr
+++ src/completable.cr
@@ -2,7 +2,13 @@ require "./transformer"
 require "./operator"
 require "./observer"
 
+abstract class CompletableSource(T)
+  abstract def subscribe(observer : CompletableObserver(T))
+end
+
 abstract class Completable
+  include CompletableSource(T)
+
   def |(transformer : CompletableTransformer) : Completable
     transformer.apply(self)
   end
