23c3709f9d65585a36359e08bebbaf31351dd663
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 11:35:13 +0800

    Create LambdaMaybeObserver.cr

---
+++ src/observers/single/LambdaMaybeObserver.cr
@@ -0,0 +1,59 @@
+require "../../observer"
+require "../../cancellable"
+require "../../subscription"
+
+class LambdaSingleObserver(T)
+  include SingleObserver(T)
+  include Cancellable
+
+  @onSuccess : Proc(T, Nil)
+  @onError : Proc(Exception, Nil)
+
+  @withSubscription : Bool
+  @state : Subscription
+  @alive : Bool
+
+  def initialize(@onSuccess : Proc(T, Nil), @onError : Proc(Exception, Nil))
+    @state = BasicSubscription.new
+    @withSubscription = false
+    @alive = true
+  end
+
+  def cancel
+    if (@alive)
+      @alive = false
+      @state.cancel()
+    end
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+  def onSuccess(x : T)
+    if (@withSubscription && @alive)
+      begin
+        @onSuccess.call(x)
+      ensure
+        self.cancel()
+      end
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      begin
+        @onError.call(e)
+      ensure
+        self.cancel()
+      end
+    else
+      raise e
+    end
+  end
+end
