22ad5abe97e6ce9d86be09d326f53f1377ac2bbc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-28 12:26:33 +0800

    Update emitter.cr

--- src/emitter.cr
+++ src/emitter.cr
@@ -1,31 +1,31 @@
 require "./cancellable"
 
 module SingleEmitter(T)
-  abstract def setCancellable(cancellable : Cancellable)
+  abstract def addCleanup(cleanup : Proc(Void))
   abstract def onSuccess(x : T)
   abstract def onError(e : Exception)
-  abstract def isCancelled
+  abstract def isCancelled : Bool
 end
 
 module CompletableEmitter
-  abstract def setCancellable(cancellable : Cancellable)
+  abstract def addCleanup(cleanup : Proc(Void))
   abstract def onComplete
   abstract def onError(e : Exception)
-  abstract def isCancelled
+  abstract def isCancelled : Bool
 end
 
 module MaybeEmitter(T)
-  abstract def setCancellable(cancellable : Cancellable)
+  abstract def addCleanup(cleanup : Proc(Void))
   abstract def onComplete
   abstract def onSuccess(x : T)
   abstract def onError(e : Exception)
-  abstract def isCancelled
+  abstract def isCancelled : Bool
 end
 
 module ObservableEmitter(T)
-  abstract def setCancellable(cancellable : Cancellable)
+  abstract def addCleanup(cleanup : Proc(Void))
   abstract def onComplete
   abstract def onError(e : Exception)
   abstract def onNext(x : T)
-  abstract def isCancelled
+  abstract def isCancelled : Bool
 end
