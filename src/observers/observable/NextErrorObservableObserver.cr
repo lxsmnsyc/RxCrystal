7cf070c15859613ff683fb47d15897eef167924c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-29 10:43:34 +0800

    Create NextErrorObservableObserver.cr

---
+++ src/observers/observable/NextErrorObservableObserver.cr
@@ -0,0 +1,62 @@
+require "../../ObservableObserver"
+require "../../Subscription"
+require "../../subscriptions/BasicSubscription"
+
+class NextErrorObservableObserver(T)
+  include ObservableObserver(T)
+  include Subscription
+
+  @onNext : (Proc(T, Nil))?
+  @onError : (Proc(Exception, Nil))?
+  @withSubscription : Bool
+  @state : Subscription
+  @alive : Bool
+
+  def initialize(@onNext : Proc(T, Nil), @onError : Proc(Exception, Nil))
+    @state = BasicSubscription.new
+    @withSubscription = false
+    @alive = true
+  end
+
+  def cancel
+    if (@alive)
+      @alive = false
+      @state.cancel()
+    end
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+  def onNext(x : T)
+    if (@withSubscription && @alive)
+      begin
+        @onNext.call(x)
+      end
+    end
+  end
+
+  def onComplete
+    if (@withSubscription && @alive)
+      self.cancel()
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      begin
+        @onError.call(e)
+      ensure
+        self.cancel()
+      end
+    else
+      raise e
+    end
+  end
+end
