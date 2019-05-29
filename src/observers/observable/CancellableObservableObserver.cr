3b35483b17d4bcec79f45866b55ca92f6d43921a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-29 10:43:28 +0800

    Create CancellableObservableObserver.cr

---
+++ src/observers/observable/CancellableObservableObserver.cr
@@ -0,0 +1,65 @@
+require "../../ObservableObserver"
+require "../../Subscription"
+require "../../subscriptions/BasicSubscription"
+
+class CancellableObservableObserver(T)
+  include ObservableObserver(T)
+  include Subscription
+
+  @upstream : ObservableObserver(T)
+  @withSubscription : Bool
+  @state : Subscription
+  @alive : Bool
+
+  def initialize(@upstream : ObservableObserver(T))
+    @state = BasicSubscription.new
+    @withSubscription = false
+    @alive = true
+  end
+
+  def cancel
+    if (@alive)
+      @alive = false
+      @state.cancel()
+    end
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+  def onNext(x : T)
+    if (@withSubscription && @alive)
+      begin
+        @upstream.onNext(x)
+      end
+    end
+  end
+
+  def onComplete
+    if (@withSubscription && @alive)
+      begin
+        @upstream.onComplete()
+      ensure
+        self.cancel()
+      end
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      begin
+        @upstream.onError(e)
+      ensure
+        self.cancel()
+      end
+    else
+      raise e
+    end
+  end
+end
