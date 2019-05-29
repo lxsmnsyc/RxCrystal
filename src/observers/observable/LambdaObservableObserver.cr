a74f312b8cf9d3fd44d7d7ab66a10dbb50c8cf55
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-29 10:43:32 +0800

    Create LambdaObservableObserver.cr

---
+++ src/observers/observable/LambdaObservableObserver.cr
@@ -0,0 +1,68 @@
+require "../../ObservableObserver"
+require "../../Subscription"
+require "../../subscriptions/BasicSubscription"
+
+class LambdaObservableObserver(T)
+  include ObservableObserver(T)
+  include Subscription
+
+  @onComplete : (Proc(Void))?
+  @onSuccess : (Proc(T, Nil))?
+  @onError : (Proc(Exception, Nil))?
+
+  @withSubscription : Bool
+  @state : Subscription
+  @alive : Bool
+
+  def initialize(@onSuccess : Proc(T, Nil), @onComplete : Proc(Void), @onError : Proc(Exception, Nil))
+    @state = BasicSubscription.new
+    @withSubscription = false
+    @alive = true
+  end
+
+  def cancel
+    if (@alive)
+      @alive = false
+      @state.cancel()
+    end
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+  def onNext(x : T)
+    if (@withSubscription && @alive)
+      begin
+        @onSuccess.call(x)
+      end
+    end
+  end
+
+  def onComplete
+    if (@withSubscription && @alive)
+      begin
+        @onComplete.call()
+      ensure
+        self.cancel()
+      end
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      begin
+        @onError.call(e)
+      ensure
+        self.cancel()
+      end
+    else
+      raise e
+    end
+  end
+end
