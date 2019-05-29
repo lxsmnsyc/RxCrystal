a0dac67950c1ac7c56b29b4d961050993e1fb23d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-29 14:29:08 +0800

    Update CompletableObserver.cr

--- src/CompletableObserver.cr
+++ src/CompletableObserver.cr
@@ -25,9 +25,9 @@
 # author Alexis Munsayac <alexis.munsayac@gmail.com>
 # copyright Alexis Munsayac 2019
 #
-
+require "./Subscription"
 module CompletableObserver
-  abstract def onSubscribe(c : Cancellable)
+  abstract def onSubscribe(c : Subscription)
   abstract def onError(x : Exception)
   abstract def onComplete
 end
