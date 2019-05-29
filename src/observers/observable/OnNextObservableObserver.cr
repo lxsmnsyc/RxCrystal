801f1c0c9df41a4acd96380a56552718cf470064
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-29 10:43:43 +0800

    Create OnNextObservableObserver.cr

---
+++ src/observers/observable/OnNextObservableObserver.cr
@@ -0,0 +1,57 @@
+require "../../ObservableObserver"
+require "../../Subscription"
+require "../../subscriptions/BasicSubscription"
+
+class OnNextObservableObserver(T)
+  include ObservableObserver(T)
+  include Subscription
+
+  @upstream : (Proc(T, Nil))?
+  @withSubscription : Bool
+  @state : Subscription
+  @alive : Bool
+
+  def initialize(@upstream : Proc(T, Nil))
+    @state = BasicSubscription.new
+    @withSubscription = false
+    @alive = true
+  end
+
+  def cancel
+    if (@alive)
+      @alive = false
+      @state.cancel()
+    end
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+  def onNext(x : T)
+    if (@withSubscription && @alive)
+      begin
+        @upstream.call(x)
+      end
+    end
+  end
+
+  def onComplete
+    if (@withSubscription && @alive)
+      self.cancel()
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      self.cancel()
+    else
+      raise e
+    end
+  end
+end
