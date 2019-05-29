7cbba8cb614130d3a101ee5e08a9eb8766d3b409
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-29 10:43:40 +0800

    Create OnErrorObservableObserver.cr

---
+++ src/observers/observable/OnErrorObservableObserver.cr
@@ -0,0 +1,58 @@
+require "../../ObservableObserver"
+require "../../Subscription"
+require "../../subscriptions/BasicSubscription"
+
+class OnErrorObservableObserver(T)
+  include ObservableObserver(T)
+  include Subscription
+
+  @upstream : (Proc(Exception, Nil))?
+  @withSubscription : Bool
+  @state : Subscription
+  @alive : Bool
+
+  def initialize(@upstream : Proc(Exception, Nil))
+    @state = BasicSubscription.new
+    @withSubscription = false
+    @alive = true
+  end
+
+  def cancel
+    if (@alive)
+      @alive = false
+      @state.cancel()
+    end
+  end
+
+  def onSubscribe(x : Subscription)
+    if (@withSubscription)
+      x.cancel()
+    else
+      @withSubscription = true
+      @state = x
+    end
+  end
+
+  def onNext(x : T)
+    if (@withSubscription && @alive)
+    end
+  end
+
+  def onComplete
+    if (@withSubscription && @alive)
+      self.cancel()
+    end
+  end
+
+  def onError(e : Exception)
+    if (@withSubscription && @alive)
+      begin
+        @upstream.call(e)
+      ensure
+        self.cancel()
+      end
+    else
+      raise e
+    end
+  end
+end
