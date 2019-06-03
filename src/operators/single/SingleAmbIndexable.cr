447580c7e0a22c72af63363bb1d0d5539f0b14b4
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-03 10:47:24 +0800

    Update SingleAmbIndexable.cr

--- src/operators/single/SingleAmbIndexable.cr
+++ src/operators/single/SingleAmbIndexable.cr
@@ -1,3 +1,29 @@
+#
+# MIT License
+#
+# Copyright (c) 2019 Alexis Munsayac
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+# author Alexis Munsayac <alexis.munsayac@gmail.com>
+# copyright Alexis Munsayac 2019
+#
 require "../../Single"
 require "../../SingleSource"
 require "../../SingleObserver"
@@ -37,6 +63,7 @@ private class SingleAmbObserver(T)
   end
 end
 
+# :nodoc:
 class SingleAmbIndexable(T) < Single(T)
   def initialize(@sources : Indexable(SingleSource(T)))
   end
